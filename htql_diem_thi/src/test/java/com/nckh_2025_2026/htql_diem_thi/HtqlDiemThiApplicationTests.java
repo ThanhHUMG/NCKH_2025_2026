43eb3bb6efcefbb0e92aa926ea7d74c8b94becf5
Mai Van Thanh <maivanthanh36tb@gmail.com>
2026-03-20 19:13:07 +0700

    first commit

---
+++ htql_diem_thi/src/test/java/com/nckh_2025_2026/htql_diem_thi/HtqlDiemThiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nckh_2025_2026.htql_diem_thi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtqlDiemThiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
